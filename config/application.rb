ed5fadfe935455e77765354077de5a8feb3def39
DC <dachang.h@gmail.com>
2018-07-04 18:43:25 +0800

    步驟10: 設定 Rails 的時區

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TaskApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.available_locales = %i[en zh-TW]
+    config.time_zone = 'Taipei'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
