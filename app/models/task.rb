abe8104724dd79462baf3161b3acb77807f41edb
DC <dachang.h@gmail.com>
2018-07-12 14:57:06 +0800

    步驟16: 設定優先順序

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
-  validates :title, :description, :end_at, :status ,presence: true  
+  validates :title, :description, :end_at, :status, :priority ,presence: true  
 
   enum status: [:pending, :ongoing, :finished]
+  enum priority: [:low, :medium, :high]
+
 end
--- spec/features/tasks_orders_spec.rb
+++ spec/features/tasks_orders_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "TasksOrders", type: :feature do
   
-  let!(:first_task) { Task.create(title: 'first_title', description: 'first_desc', end_at: Time.now, status: 0) }
-  let!(:new_task) { Task.create(title: 'new_title', description: 'new_desc', end_at: Time.now + 1.day, status: 1) }
+  let!(:first_task) { Task.create(title: 'first_title', description: 'first_desc', end_at: Time.now, status: 0, priority: 0) }
+  let!(:new_task) { Task.create(title: 'new_title', description: 'new_desc', end_at: Time.now + 1.day, status: 1, priority: 1) }
 
   describe '步驟11' do
     it '任務列表以建立時間排序' do
@@ -52,6 +52,16 @@ RSpec.feature "TasksOrders", type: :feature do
       click_button(I18n.t('buttons.search'))
       expect(page).to have_content(first_task.title)
       expect(page).to have_no_content(new_task.title)
+    end
+  end
+
+  describe '步驟16' do
+    it '能夠以優先順序做排序' do
+      visit '/tasks'
+      click_link(I18n.t('activerecord.attributes.task.priority'))
+      expect(page.body.index(first_task.title)).to be < page.body.index(new_task.title)
+      click_link(I18n.t('activerecord.attributes.task.priority'))
+      expect(page.body.index(first_task.title)).to be > page.body.index(new_task.title)
     end
   end
 
